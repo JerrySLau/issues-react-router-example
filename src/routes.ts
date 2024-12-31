b81b1143740fa437e79da1cbd08e85ef02e585f6
Jerry <liushipu@icloud.com>
2024-12-31 09:38:12 +0800

    add fs-route

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,10 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+// import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/_index.tsx")] satisfies RouteConfig;
+// export default [index("routes/_index.tsx")] satisfies RouteConfig;
+
+import { type RouteConfig } from "@react-router/dev/routes";
+import { flatRoutes } from "@react-router/fs-routes";
+
+export default flatRoutes({
+  rootDirectory: "pages",
+}) satisfies RouteConfig;
