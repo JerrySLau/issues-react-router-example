699cc9722dc40aedb6f3d08905c8a1f11c912cbd
Jerry <liushipu@icloud.com>
2024-12-31 11:10:35 +0800

    add react({ jsxImportSource: '@emotion/react' })

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    react({ jsxImportSource: '@emotion/react' }),
+    reactRouter(),
+    tsconfigPaths()
+  ],
 });
